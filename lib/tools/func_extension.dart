765dd1e9302f0a48ec72496b3c7ae78a3def7ddb
Empty <2167335204@qq.com>
2025-07-04 16:09:25 +0800

    1

--- lib/tools/func_extension.dart
+++ lib/tools/func_extension.dart
@@ -23,3 +23,19 @@ extension DebounceExtension on Function {
     };
   }
 }
+
+bool _throttleIsAllowed = true;
+extension ThrottleExtension on Function {
+  void Function() throttle({int ms = 500}) {
+    Timer? throttleTimer;
+    return () {
+      if (!_throttleIsAllowed) return;
+      _throttleIsAllowed = false;
+      this();
+      throttleTimer?.cancel();
+      throttleTimer = Timer(Duration(milliseconds: ms), () {
+        _throttleIsAllowed = true;
+      });
+    };
+  }
+}
