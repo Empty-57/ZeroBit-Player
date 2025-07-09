2972db55bce3e2ef59dab6f703ab813cbcab7526
Empty <2167335204@qq.com>
2025-07-09 21:32:06 +0800

    1

--- lib/field/audio_source.dart
+++ lib/field/audio_source.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 
 class AudioSource extends GetxController {
   static const allMusic = 'allMusic';
-  static const artist = 'artist';
 
   final currentAudioSource = AudioSource.allMusic.obs;
 }
