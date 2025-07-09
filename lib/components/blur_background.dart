53d8eaf813440ab0d84f8978ad0229e2ee291a36
Empty <2167335204@qq.com>
2025-07-09 21:12:47 +0800

    1

---
+++ lib/components/blur_background.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import '../tools/audio_ctrl_mixin.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BlurBackground extends StatelessWidget {
+  final AudioControllerGenClass controller;
+  final Widget widget;
+
+  const BlurBackground({
+    super.key,
+    required this.controller,
+    required this.widget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(8)),
+          child: Container(color: Theme.of(context).colorScheme.surface),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(8)),
+          child: ImageFiltered(
+            imageFilter: ImageFilter.blur(sigmaX: 48, sigmaY: 48),
+            child: ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    Colors.white.withValues(alpha: 0.4),
+                    Colors.transparent,
+                  ],
+                  tileMode: TileMode.clamp,
+                ).createShader(bounds);
+              },
+              child: Obx(
+                () => AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  switchInCurve: Curves.easeIn,
+                  switchOutCurve: Curves.easeOut,
+                  child: SizedBox.expand(
+                    child: Image.memory(
+                      controller.headCover.value,
+                      key: ValueKey(controller.headCover.value.hashCode),
+                      cacheWidth: 800,
+                      cacheHeight: 800,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  transitionBuilder: (Widget child, Animation<double> anim) {
+                    return FadeTransition(opacity: anim, child: child);
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+        widget,
+      ],
+    );
+  }
+}
--- lib/pages/playlist_page.dart
+++ lib/pages/playlist_page.dart
@@ -1,8 +1,8 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:zerobit_player/HIveCtrl/models/user_playlist_model.dart';
 import 'package:zerobit_player/components/audio_gen_pages.dart';
+import '../components/blur_background.dart';
 import '../field/operate_area.dart';
 import '../getxController/play_list_ctrl.dart';
 import '../field/tag_suffix.dart';
@@ -12,7 +12,8 @@ class PlayList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final UserPlayListCache userArgs = ModalRoute.of(context)!.settings.arguments as UserPlayListCache;
+    final UserPlayListCache userArgs =
+        ModalRoute.of(context)!.settings.arguments as UserPlayListCache;
 
     late final PlayListController playListController;
 
@@ -22,66 +23,15 @@ class PlayList extends StatelessWidget {
       tag: userArgs.userKey,
     );
 
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(8)),
-          child: Container(
-            color: Theme.of(context).colorScheme.surface,
-          ),
-        ),
-        ClipRRect(
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(8)),
-          child: ImageFiltered(
-            imageFilter: ImageFilter.blur(sigmaX: 48, sigmaY: 48),
-            child: ShaderMask(
-              shaderCallback: (Rect bounds) {
-                return LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: <Color>[
-                    Colors.white.withValues(alpha: 0.4),
-                    Colors.transparent,
-                  ],
-                  tileMode: TileMode.clamp,
-                ).createShader(bounds);
-              },
-              child: Obx(
-                  () => AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 300),
-                      switchInCurve: Curves.easeIn,
-                      switchOutCurve: Curves.easeOut,
-                      child: SizedBox.expand(
-                        child: Image.memory(
-                        playListController.headCover.value,
-                        key: ValueKey(
-                          playListController.headCover.value.hashCode,
-                        ),
-                        cacheWidth: 800,
-                        cacheHeight: 800,
-                        fit: BoxFit.fill,
-                      ),
-                      ),
-                      transitionBuilder: (
-                        Widget child,
-                        Animation<double> anim,
-                      ) {
-                        return FadeTransition(opacity: anim, child: child);
-                      },
-                    ),
-                ),
-            ),
-          ),
-        ),
-
-        AudioGenPages(
-          title: userArgs.userKey.split(TagSuffix.playList)[0],
-          operateArea: OperateArea.playList,
-          audioSource: userArgs.userKey,
-          controller: playListController,
-          backgroundColor: Colors.transparent,
-        ),
-      ],
+    return BlurBackground(
+      controller: playListController,
+      widget: AudioGenPages(
+        title: userArgs.userKey.split(TagSuffix.playList)[0],
+        operateArea: OperateArea.playList,
+        audioSource: userArgs.userKey,
+        controller: playListController,
+        backgroundColor: Colors.transparent,
+      ),
     );
   }
 }
