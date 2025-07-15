045339b03e79735af8b4149c5f8f142512338390
Empty <2167335204@qq.com>
2025-07-15 16:50:48 +0800

    1

--- lib/tools/sync_cache.dart
+++ lib/tools/sync_cache.dart
@@ -1,17 +1,16 @@
 import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
-import 'package:zerobit_player/HIveCtrl/models/music_cache_model.dart';
+import 'package:path/path.dart' as p;
 
 import '../HIveCtrl/hive_manager.dart';
+import '../HIveCtrl/models/music_cache_model.dart';
 import '../getxController/audio_ctrl.dart';
 import '../getxController/music_cache_ctrl.dart';
 import '../getxController/setting_ctrl.dart';
 import '../src/rust/api/music_tag_tool.dart';
-import 'package:path/path.dart' as p;
 
-const _supportedExts = [
+const Set<String> _supportedExts = {
   '.aac',
   '.ape',
   '.aiff',
@@ -26,70 +25,87 @@ const _supportedExts = [
   '.spx',
   '.wav',
   '.wv',
-];
+};
 
-Future<void> syncCache() async {
-  final audioPaths = <String>[];
-  final musicCacheBox = HiveManager.musicCacheBox;
-  final SettingController settingController = Get.find<SettingController>();
-  final MusicCacheController musicCacheController = Get.find<MusicCacheController>();
-  final AudioController audioController = Get.find<AudioController>();
-
-  final folders=settingController.folders;
-
-  try {
-    for (final dir in folders) {
-      final d = Directory(dir);
-      await for (final entity in d.list(recursive: true)) {
-        if (entity is File) {
-          if (_supportedExts.contains(p.extension(entity.path).toLowerCase())) {
-            audioPaths.add(entity.path);
-          }
-        }
+Future<Set<String>> _scanAudioPaths(List<String> folders) async {
+  final Set<String> paths = {};
+  for (final dirPath in folders) {
+    final directory = Directory(dirPath);
+    if (!await directory.exists()) continue;
+    await for (final entity in directory.list(
+      recursive: true,
+      followLinks: false,
+    )) {
+      if (entity is File &&
+          _supportedExts.contains(p.extension(entity.path).toLowerCase())) {
+        paths.add(entity.path);
       }
     }
-  } catch (err) {
-    debugPrint(err.toString());
   }
+  return paths;
+}
 
-  final musicKeys = musicCacheBox.getKeyAll();
-
-  final Map<String, MusicCache> tagBuffer = {};
-  final removeKeys=<String>[];
-
-  for (final path in audioPaths) {
-    if (!musicKeys.contains(path)) {
-      final metaData = await getMetadata(path: path);
-      tagBuffer[path] = MusicCache(
-        artist: metaData.artist,
-        album: metaData.album,
-        title: metaData.title,
-        genre: metaData.genre,
-        duration: metaData.duration,
-        bitrate: metaData.bitrate,
-        sampleRate: metaData.sampleRate,
-        path: metaData.path,
+Future<Map<String, MusicCache>> _fetchMetadataBatch(
+  Set<String> newPaths,
+  ValueSetter<String> onScanProgress,
+) async {
+  final futures = newPaths.map((path) async {
+    try {
+      onScanProgress(path);
+      final meta = await getMetadata(path: path);
+      return MapEntry(
+        path,
+        MusicCache(
+          artist: meta.artist,
+          album: meta.album,
+          title: meta.title,
+          genre: meta.genre,
+          duration: meta.duration,
+          bitrate: meta.bitrate,
+          sampleRate: meta.sampleRate,
+          path: meta.path,
+        ),
       );
-
-      musicCacheController.currentScanPath.value=path;
-
+    } catch (e) {
+      debugPrint('Metadata error for $path: \$e');
+      return null;
     }
+  });
+
+  final entries = await Future.wait(futures);
+  return Map.fromEntries(entries.whereType<MapEntry<String, MusicCache>>());
+}
+
+/// 同步本地音乐缓存
+Future<void> syncCache() async {
+  final settingCtrl = Get.find<SettingController>();
+  final musicCacheCtrl = Get.find<MusicCacheController>();
+  final audioCtrl = Get.find<AudioController>();
+  final musicBox = HiveManager.musicCacheBox;
+
+  final scannedPaths = await _scanAudioPaths(settingCtrl.folders);
+
+  final existingKeys = Set<String>.from(musicBox.getKeyAll());
+
+  final newPaths = scannedPaths.difference(existingKeys);
+  final removedPaths = existingKeys.difference(scannedPaths);
+
+  if (removedPaths.isNotEmpty) {
+    await musicBox.delAll(keyList: removedPaths.toList());
   }
 
-  for(final key in musicKeys){
-    if(!audioPaths.contains(key)){
-      removeKeys.add(key);
-    }
+  if (newPaths.isNotEmpty) {
+    final tagBuffer = await _fetchMetadataBatch(
+      newPaths,
+      (path) => musicCacheCtrl.currentScanPath.value = path,
+    );
+    await musicBox.putAll(data: tagBuffer);
   }
 
-  await musicCacheBox.delAll(keyList:removeKeys);
-  await musicCacheBox.putAll(data:tagBuffer);
+  musicCacheCtrl.currentScanPath.value = '';
+  musicCacheCtrl.items.clear();
+  musicCacheCtrl.loadData();
 
-  musicCacheController.items.clear();
-  musicCacheController.loadData();
-
-  audioController.syncPlayListCacheItems();
-  audioController.syncCurrentIndex();
-  musicCacheController.currentScanPath.value='';
-
+  audioCtrl.syncPlayListCacheItems();
+  audioCtrl.syncCurrentIndex();
 }
