74b6d96a75a37c4acaac07eaf1fae61c00b316ee
Empty-57 <2167335204@qq.com>
2026-04-25 16:25:56 +0800

    更新api

--- lib/API/apis.dart
+++ lib/API/apis.dart
@@ -8,64 +8,73 @@ import '../getxController/setting_ctrl.dart';
 import '../tools/lrcTool/lyric_model.dart';
 import '../tools/qrc_decryptor.dart';
 
-const _qmSearchUrl = "https://c.y.qq.com/soso/fcgi-bin/client_search_cp";
 const _neSearchUrl = "https://music.163.com/api/cloudsearch/pc";
-
-const _qmLrcUrl = "https://c.y.qq.com/qqmusic/fcgi-bin/lyric_download.fcg";
 const _neLrcUrl = "https://music.163.com/api/song/lyric";
 
-const _coverSize=800; // 150, 300, 500, 800
+const _qmSearchUrl = "https://u.y.qq.com/cgi-bin/musicu.fcg";
+const _qmLrcUrl = "https://c.y.qq.com/qqmusic/fcgi-bin/lyric_download.fcg";
 
-final _dio = Dio(BaseOptions(
+const _coverSize = 800; // 150, 300, 500, 800
+
+final _dio = Dio(
+  BaseOptions(
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+      'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
       'Connection': 'keep-alive',
     },
-  ));
+  ),
+);
+
+final _qmDio = Dio(
+  BaseOptions(
+    headers: {
+      "Host": "u.y.qq.com",
+      'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+      'Connection': 'keep-alive',
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  ),
+);
 
 final SettingController _settingController = Get.find<SettingController>();
 
-Future<dynamic> _saveNetCover({required String songPath, required String picUrl,required bool saveCover}) async {
-  try{
+Map<String, String?> parseLyricByRegex(String rawXml) {
+  String? extract(String tagName) {
+    // 匹配标签内的 CDATA 内容
+    // 允许标签有属性，匹配 <tag ...><![CDATA[内容]]></tag>
+    final regExp = RegExp(
+      '<$tagName[^>]*><!\\[CDATA\\[([\\s\\S]*?)\\]\\]><\\/$tagName>',
+      caseSensitive: false,
+    );
+    final match = regExp.firstMatch(rawXml);
+    return match?.group(1);
+  }
+
+  return {
+    'lyric': extract('content'),
+    'trans': extract('contentts'),
+    'roma': extract('contentroma'),
+  };
+}
+
+Future<dynamic> _saveNetCover({
+  required String songPath,
+  required String picUrl,
+  required bool saveCover,
+}) async {
+  try {
     final pic = await _dio.get(
-    picUrl,
-    options: Options(responseType: ResponseType.bytes),
-  );
+      picUrl,
+      options: Options(responseType: ResponseType.bytes),
+    );
 
     if (pic.data != null) {
-      if(saveCover){
+      if (saveCover) {
         await editCover(path: songPath, src: Uint8List.fromList(pic.data));
       }
-    return pic.data;
-  }
-
-  }catch(e){
-    debugPrint('$e,$picUrl');
-  }
-  return null;
-}
-
-Future<dynamic> _qmSearchByText({required String text, required int offset, required int limit,}) async {
-  final response = await _dio.get(
-    _qmSearchUrl,
-    queryParameters: {"format": 'json', "w": text, "n": limit, "p": offset},
-    options: Options(responseType: ResponseType.plain),
-  );
-  if (response.data != null) {
-    return jsonDecode(response.data);
-  }
-  return null;
-}
-
-Future<dynamic> _qmSaveCoverByText({required String text, required String songPath,bool? saveCover=true}) async {
-  String? picUrl;
-  try {
-    final Map<String, dynamic> data = await _qmSearchByText(text: text, offset: 1, limit: 1);
-    final midList = data["data"]?["song"]?["list"];
-    final String? mid = midList!=null ? (midList[0]?["albummid"]).toString().trim():null;
-    if (mid != null && mid.isNotEmpty) {
-      picUrl = "https://y.gtimg.cn/music/photo_new/T002R${_coverSize}x${_coverSize}M000$mid.jpg";
-      return await _saveNetCover(songPath: songPath, picUrl: picUrl,saveCover: saveCover!);
+      return pic.data;
     }
   } catch (e) {
     debugPrint('$e,$picUrl');
@@ -73,95 +82,188 @@ Future<dynamic> _qmSaveCoverByText({required String text, required String songPa
   return null;
 }
 
-Future<Get4NetLrcModel?> _qmGetLrc({required int id})async{
-  final response = await _dio.get(
+Future<dynamic> _qmSearchByText({
+  required String text,
+  required int offset,
+  required int limit,
+}) async {
+  final response = await _qmDio.post(
+    _qmSearchUrl,
+    data: jsonEncode({
+      "comm": {"ct": "19", "cv": "1873", "uin": "0"},
+      "music.search.SearchCgiService": {
+        "method": "DoSearchForQQMusicDesktop",
+        "module": "music.search.SearchCgiService",
+        "param": {
+          "grp": 1,
+          "num_per_page": limit,
+          "page_num": offset,
+          "query": text,
+          "search_type": 0,
+        },
+      },
+    }),
+    options: Options(responseType: ResponseType.bytes), // 接口返回的数据不规范，防止报错
+  );
+  if (response.data != null) {
+    final rawString = utf8.decode(response.data as List<int>);
+    return jsonDecode(rawString);
+  }
+  return null;
+}
+
+Future<dynamic> _qmSaveCoverByText({
+  required String text,
+  required String songPath,
+  bool? saveCover = true,
+}) async {
+  String? picUrl;
+  try {
+    final Map<String, dynamic> data = await _qmSearchByText(
+      text: text,
+      offset: 1,
+      limit: 1,
+    );
+    final songList =
+        data["music.search.SearchCgiService"]?["data"]?["body"]?["song"]?["list"];
+    if (songList is! List || songList.isEmpty) {
+      return null;
+    }
+    final mid = songList.first?['album']?['mid']?.toString().trim();
+    if (mid != null && mid.isNotEmpty) {
+      picUrl =
+          "https://y.gtimg.cn/music/photo_new/T002R${_coverSize}x${_coverSize}M000$mid.jpg";
+      return await _saveNetCover(
+        songPath: songPath,
+        picUrl: picUrl,
+        saveCover: saveCover!,
+      );
+    }
+  } catch (e) {
+    debugPrint('$e,$picUrl');
+  }
+  return null;
+}
+
+Future<Get4NetLrcModel?> _qmGetLrc({required int id}) async {
+  final response = await _qmDio.get(
     _qmLrcUrl,
-    queryParameters: {
-      "version":'15',
-      "lrctype":'4',
-      "musicid":id,
-    },
+    queryParameters: {"version": '15', "lrctype": '4', "musicid": id},
     options: Options(responseType: ResponseType.plain),
   );
 
   final String? body = response.data?.toString();
   if (body == null || body.isEmpty) {
-    return Get4NetLrcModel(lrc: null,verbatimLrc: null,translate: null,type: LyricFormat.qrc);
+    return Get4NetLrcModel(
+      lrc: null,
+      verbatimLrc: null,
+      translate: null,
+      type: LyricFormat.qrc,
+    );
   }
 
   //Original、ts、roma
-  final regex = RegExp(r'CDATA\[(\S+)]]');//CDATA\[(\S+?)\]\]
-  final List<String> extracted = regex
-      .allMatches(body)
-      .map((m) => m.group(1))
-      .whereType<String>()
-      .toList();
-
-  final String? encryptedOriginal = extracted.isNotEmpty ? extracted[0] : null;
-  final String? encryptedTranslate =
-      extracted.length > 1 ? extracted[1] : null;
+  final data = parseLyricByRegex(body);
+  String? encryptedOriginal = data['lyric'];
+  String? encryptedTranslate = data['trans'];
+  String? decryptedRoma = data['roma'];
 
   String? qrcDecrypted;
   String? translateDecrypted;
 
   if (encryptedOriginal != null && encryptedOriginal.isNotEmpty) {
-    try {
-      qrcDecrypted =
-          await qrcDecrypt(encryptedQrc: encryptedOriginal, isLocal: false);
-    } catch (e) {
-      debugPrint('qrcDecrypt original error: $e');
-      qrcDecrypted = null;
+    if (!encryptedOriginal.trimLeft().startsWith('<?xml') &&
+        !encryptedOriginal.trimLeft().startsWith('<Qrc')) {
+      // 只要以以上俩字符串开头就代表已解压
+      try {
+        qrcDecrypted = await qrcDecrypt(
+          encryptedQrc: encryptedOriginal,
+          isLocal: false,
+        );
+      } catch (e) {
+        debugPrint('qrcDecrypt original error: $e');
+        qrcDecrypted = null;
+      }
     }
   }
 
   if (encryptedTranslate != null && encryptedTranslate.isNotEmpty) {
-    try {
-      translateDecrypted =
-          await qrcDecrypt(encryptedQrc: encryptedTranslate, isLocal: false);
-    } catch (e) {
-      debugPrint('qrcDecrypt translate error: $e');
-      translateDecrypted = null;
+    if (!encryptedTranslate.contains("[00") &&
+        !encryptedTranslate.contains("[al")) {
+      // 只要包含时间戳或者专辑信息就不解密
+      try {
+        translateDecrypted = await qrcDecrypt(
+          encryptedQrc: encryptedTranslate,
+          isLocal: false,
+        );
+      } catch (e) {
+        debugPrint('qrcDecrypt translate error: $e');
+        translateDecrypted = null;
+      }
+    } else {
+      translateDecrypted = encryptedTranslate;
     }
   }
 
-  return Get4NetLrcModel(lrc: null,verbatimLrc: qrcDecrypted,translate: translateDecrypted,type: LyricFormat.qrc);
+  return Get4NetLrcModel(
+    lrc: null,
+    verbatimLrc: qrcDecrypted,
+    translate: translateDecrypted,
+    type: LyricFormat.qrc,
+  );
 }
 
-Future<List<SearchLrcModel?>> _qmGetLrcBySearch({required String text,required int offset,required int limit})async{
-final List<SearchLrcModel> lrcData=[];
+Future<List<SearchLrcModel?>> _qmGetLrcBySearch({
+  required String text,
+  required int offset,
+  required int limit,
+}) async {
+  final List<SearchLrcModel> lrcData = [];
   try {
-    final Map<String, dynamic> data=await _qmSearchByText(text: text,offset: offset,limit: limit);
-    final songList = data["data"]?["song"]?["list"];
+    final Map<String, dynamic> data = await _qmSearchByText(
+      text: text,
+      offset: offset,
+      limit: limit,
+    );
+    final songList =
+        data["music.search.SearchCgiService"]?["data"]?["body"]?["song"]?["list"];
     if (songList is! List || songList.isEmpty) {
       return [];
     }
 
     for (final item in songList) {
-      final data=await _qmGetLrc(id:item["songid"]);
-      if (data==null){
+      final data = await _qmGetLrc(id: item["id"]);
+      if (data == null) {
         continue;
       }
 
-      var singerList=item["singer"];
+      var singerList = item["singer"];
       String? singer;
-      if(singerList is List&&singerList.isNotEmpty){
-        singer=singerList[0]["name"];
+      if (singerList is List && singerList.isNotEmpty) {
+        singer = singerList[0]["name"];
       }
 
-      lrcData.add(SearchLrcModel(title: item["songname"]??'UNKNOWN',artist: singer??'UNKNOWN',id: item["songid"]??'UNKNOWN',lyric: data));
+      lrcData.add(
+        SearchLrcModel(
+          title: item["title"] ?? 'UNKNOWN',
+          artist: singer ?? 'UNKNOWN',
+          id: item["id"] ?? 'UNKNOWN',
+          lyric: data,
+        ),
+      );
     }
     return lrcData;
-  }catch (err){
+  } catch (err) {
     debugPrint(err.toString());
-    return [];
+    return lrcData;
   }
 }
 
-
-
-
-
-Future<dynamic> _neSearchByText({required String text, required int offset, required int limit,}) async {
+Future<dynamic> _neSearchByText({
+  required String text,
+  required int offset,
+  required int limit,
+}) async {
   final response = await _dio.get(
     _neSearchUrl,
     queryParameters: {
@@ -179,20 +281,31 @@ Future<dynamic> _neSearchByText({required String text, required int offset, requ
   return null;
 }
 
-Future<dynamic> _neSaveCoverByText({required String text, required String songPath,bool? saveCover=true}) async {
+Future<dynamic> _neSaveCoverByText({
+  required String text,
+  required String songPath,
+  bool? saveCover = true,
+}) async {
   String? picUrl;
   try {
-    final Map<String, dynamic> data = await _neSearchByText(text: text, offset: 1, limit: 1);
-    final songCount=data["result"]?["songCount"];
+    final Map<String, dynamic> data = await _neSearchByText(
+      text: text,
+      offset: 1,
+      limit: 1,
+    );
+    final songCount = data["result"]?["songCount"];
     if (songCount is int && songCount > 0) {
-
-      final songList=data["result"]?["songs"];
-      if(songList is! List || songList.isEmpty) {
+      final songList = data["result"]?["songs"];
+      if (songList is! List || songList.isEmpty) {
         return null;
       }
 
       final picUrl = songList[0]["al"]["picUrl"];
-      return await _saveNetCover(songPath: songPath, picUrl: picUrl,saveCover: saveCover!);
+      return await _saveNetCover(
+        songPath: songPath,
+        picUrl: picUrl,
+        saveCover: saveCover!,
+      );
     }
   } catch (e) {
     debugPrint('$e,$picUrl');
@@ -200,63 +313,94 @@ Future<dynamic> _neSaveCoverByText({required String text, required String songPa
   return null;
 }
 
-Future<Get4NetLrcModel?> _neGetLrc({required int id})async{
-final response = await _dio.get(
+Future<Get4NetLrcModel?> _neGetLrc({required int id}) async {
+  final response = await _dio.get(
     _neLrcUrl,
-    queryParameters: {
-      "id":id,
-      "lv":-1,
-      "yv":-1,
-      "tv":-1,
-      "os":'pc'
-    },
+    queryParameters: {"id": id, "lv": -1, "yv": -1, "tv": -1, "os": 'pc'},
     options: Options(responseType: ResponseType.plain),
   );
-final body = response.data as String?;
-if (body == null || body.isEmpty) {
-    return Get4NetLrcModel(lrc: null,verbatimLrc: null,translate: null,type: LyricFormat.lrc);
+  final body = response.data as String?;
+  if (body == null || body.isEmpty) {
+    return Get4NetLrcModel(
+      lrc: null,
+      verbatimLrc: null,
+      translate: null,
+      type: LyricFormat.lrc,
+    );
   }
   final Map<String, dynamic> data = jsonDecode(body);
 
   final String? lrcLyric = data['lrc']?['lyric'];
   final String? yrcLyric = data['yrc']?['lyric'];
-  final String? tLyric   = data['tlyric']?['lyric'];
-  final String type = yrcLyric!=null&&yrcLyric.isNotEmpty ? LyricFormat.yrc : LyricFormat.lrc;
+  final String? tLyric = data['tlyric']?['lyric'];
+  final String type =
+      yrcLyric != null && yrcLyric.isNotEmpty
+          ? LyricFormat.yrc
+          : LyricFormat.lrc;
 
-  return Get4NetLrcModel(lrc: lrcLyric,verbatimLrc: yrcLyric,translate: tLyric,type: type);
+  return Get4NetLrcModel(
+    lrc: lrcLyric,
+    verbatimLrc: yrcLyric,
+    translate: tLyric,
+    type: type,
+  );
 }
 
-Future<List<SearchLrcModel?>> _neGetLrcBySearch({required String text,required int offset,required int limit})async{
-  final List<SearchLrcModel> lrcData=[];
+Future<List<SearchLrcModel?>> _neGetLrcBySearch({
+  required String text,
+  required int offset,
+  required int limit,
+}) async {
+  final List<SearchLrcModel> lrcData = [];
   try {
-    final data=await _neSearchByText(text: text,offset: offset,limit: limit);
-    final songs=data["result"]?["songs"];
-    if (songs is! List || songs.isEmpty){
+    final data = await _neSearchByText(
+      text: text,
+      offset: offset,
+      limit: limit,
+    );
+    final songs = data["result"]?["songs"];
+    if (songs is! List || songs.isEmpty) {
       return [];
     }
 
     for (final item in songs) {
-      final data=await _neGetLrc(id:item["id"]);
-      if (data==null){
+      final data = await _neGetLrc(id: item["id"]);
+      if (data == null) {
         continue;
       }
 
-
-      lrcData.add(SearchLrcModel(title: item["name"]??'UNKNOWN',artist: item["ar"][0]["name"]??'UNKNOWN',id: item["id"]??'UNKNOWN',lyric: data));
+      lrcData.add(
+        SearchLrcModel(
+          title: item["name"] ?? 'UNKNOWN',
+          artist: item["ar"][0]["name"] ?? 'UNKNOWN',
+          id: item["id"] ?? 'UNKNOWN',
+          lyric: data,
+        ),
+      );
     }
     return lrcData;
-  }catch (err){
+  } catch (err) {
     debugPrint(err.toString());
     return [];
   }
 }
 
-
-Future<dynamic> saveCoverByText({required String text, required String songPath,bool? saveCover=true}) async {
-  return await[_qmSaveCoverByText,_neSaveCoverByText][_settingController.apiIndex.value](text: text, songPath: songPath,saveCover: saveCover);
+Future<dynamic> saveCoverByText({
+  required String text,
+  required String songPath,
+  bool? saveCover = true,
+}) async {
+  return await [_qmSaveCoverByText, _neSaveCoverByText][_settingController
+      .apiIndex
+      .value](text: text, songPath: songPath, saveCover: saveCover);
 }
 
-Future<List<SearchLrcModel?>> getLrcBySearch({required String text,required int offset,required int limit})async{
-  return await[_qmGetLrcBySearch,_neGetLrcBySearch][_settingController.apiIndex.value](text: text,offset: offset,limit: limit);
+Future<List<SearchLrcModel?>> getLrcBySearch({
+  required String text,
+  required int offset,
+  required int limit,
+}) async {
+  return await [_qmGetLrcBySearch, _neGetLrcBySearch][_settingController
+      .apiIndex
+      .value](text: text, offset: offset, limit: limit);
 }
-
