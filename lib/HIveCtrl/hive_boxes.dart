2d034573c19a375a5fedf55dec9bbd5495da30a7
Empty-57 <2167335204@qq.com>
2026-04-26 22:55:36 +0800

    因音频缓存类字段变动，更新音频盒子标签

--- lib/HIveCtrl/hive_boxes.dart
+++ lib/HIveCtrl/hive_boxes.dart
@@ -1,5 +1,5 @@
 abstract class HiveBoxes{
-  static const musicCacheBox= 'music_cache_box';
+  static const musicCacheBox= 'music_cache_box_v2';
   static const settingCacheBox='setting_box';
   static const userPlayListCacheBox='user_playlist_box';
   static const scalableSettingCacheBox='scalable_setting_box';
