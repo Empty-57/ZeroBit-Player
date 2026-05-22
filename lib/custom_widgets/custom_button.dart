641ee5a9065c7ab6590fffe42970b6b1085ee128
Empty-57 <2167335204@qq.com>
2026-05-22 22:27:49 +0800

    fix: 防止文本溢出

--- lib/custom_widgets/custom_button.dart
+++ lib/custom_widgets/custom_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zerobit_player/tools/general_style.dart';
 
-
 class CustomBtn<T> extends StatelessWidget {
   final VoidCallback fn;
   final String? label;
@@ -31,9 +30,9 @@ class CustomBtn<T> extends StatelessWidget {
     this.contentColor,
     this.backgroundColor,
     this.overlayColor,
-    this.btnWidth=128,
-    this.btnHeight=48,
-    this.spacing=6,
+    this.btnWidth = 128,
+    this.btnHeight = 48,
+    this.spacing = 6,
     this.padding,
     this.tooltip,
     this.mainAxisAlignment = MainAxisAlignment.spaceAround,
@@ -43,54 +42,64 @@ class CustomBtn<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Tooltip(
-      message: tooltip??'',
+      message: tooltip ?? '',
       child: SizedBox(
         width: btnWidth!,
         height: btnHeight!,
         child: TextButton(
-        onPressed: () {
-          fn();
-        },
-        style: TextButton.styleFrom(
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(radius!),
+          onPressed: () {
+            fn();
+          },
+          style: TextButton.styleFrom(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(radius!),
+            ),
+            overlayColor: overlayColor,
+            backgroundColor:
+                backgroundColor ??
+                Theme.of(
+                  context,
+                ).colorScheme.secondaryContainer.withValues(alpha: 1),
+            padding: padding ?? EdgeInsets.symmetric(horizontal: 8),
+            shadowColor: Colors.transparent,
           ),
-          overlayColor: overlayColor,
-          backgroundColor: backgroundColor??Theme.of(context).colorScheme.secondaryContainer.withValues(alpha: 1),
-          padding: padding??EdgeInsets.symmetric(horizontal: 8),
-          shadowColor: Colors.transparent,
+
+          child: Row(
+            mainAxisAlignment: mainAxisAlignment!,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: spacing!,
+
+            children:
+                children ??
+                [
+                  if (icon != null)
+                    Icon(
+                      icon!,
+                      color:
+                          contentColor ??
+                          Theme.of(context).colorScheme.onSecondaryContainer,
+                      size: getIconSize(size: iconSize ?? 'md'),
+                    ),
+
+                  if (label != null)
+                    Flexible(
+                      child: Text(
+                        label!,
+                        style: generalTextStyle(
+                          ctx: context,
+                          size: labelSize ?? 'md',
+                          color: contentColor,
+                        ),
+                        softWrap: false,
+                        maxLines: 1,
+                        overflow: TextOverflow.fade,
+                      ),
+                    ),
+                ],
+          ),
         ),
-
-        child: Row(
-          mainAxisAlignment: mainAxisAlignment!,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          spacing: spacing!,
-
-          children:
-              children ??
-              [
-                if (icon != null)
-                  Icon(
-                    icon!,
-                    color:
-                        contentColor??Theme.of(
-                          context,
-                        ).colorScheme.onSecondaryContainer,
-                    size:
-                        getIconSize(size: iconSize??'md'),
-                  ),
-
-                if (label != null)
-                  Text(
-                    label!,
-                    style: generalTextStyle(ctx: context,size: labelSize??'md',color: contentColor),
-                  ),
-              ],
-        ),
-      ),
       ),
     );
   }
 }
-
