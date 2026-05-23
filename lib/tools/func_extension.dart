a6936ae0b8bf3dd0c71fca2b1fc0d3911a7b7300
Empty-57 <2167335204@qq.com>
2026-05-23 23:12:49 +0800

    删除未使用的扩展方法

--- lib/tools/func_extension.dart
+++ lib/tools/func_extension.dart
@@ -1,17 +1,5 @@
 import 'dart:async';
 
-extension FutureDebounceExtension on Function {
-  void Function() futureDebounce({int ms = 500}) {
-    Timer? debounceTimer;
-    return () async {
-      if (debounceTimer?.isActive ?? false) debounceTimer?.cancel();
-      debounceTimer = Timer(Duration(milliseconds: ms), () async {
-        await this();
-      });
-    };
-  }
-}
-
 extension DebounceExtension on Function {
   void Function() debounce({int ms = 500}) {
     Timer? debounceTimer;
