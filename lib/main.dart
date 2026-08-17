e03ba5d5105737b51eb986e53b8bd4151a921ea9
Empty-57 <2167335204@qq.com>
2026-08-17 16:19:59 +0800

    减小PaintingBinding.instance.imageCache缓存数与最大大小

--- lib/main.dart
+++ lib/main.dart
@@ -148,11 +148,11 @@ void main() async {
   await windowManager.ensureInitialized();
   await RustLib.init();
 
-  // 最多缓存 200 张图片（默认1000）
-  PaintingBinding.instance.imageCache.maximumSize = 200;
+  // 最多缓存 100 张图片（默认1000）
+  PaintingBinding.instance.imageCache.maximumSize = 100;
 
-  // 最多缓存 20MB，超过就会清理（默认100MB）
-  PaintingBinding.instance.imageCache.maximumSizeBytes = 20 * 1024 * 1024;
+  // 最多缓存 10MB，超过就会清理（默认100MB）
+  PaintingBinding.instance.imageCache.maximumSizeBytes = 10 * 1024 * 1024;
 
   try {
     await loadLib();
@@ -249,7 +249,7 @@ void main() async {
   //   );
   // }
 
-  debugRepaintRainbowEnabled = false;
+  debugRepaintRainbowEnabled = true;
   _initLeakTracking();
   runApp(const MainFrame());
 
